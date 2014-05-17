d3b5e922dfb8be19afee249a4179125e402c8da5
madhuraraju <soymadhu@gmail.com>
2014-05-16 20:59:47 -0400

    Adding incomplete crap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'She-a-Bitch?' });
+});
+
+
+router.get('/about', function(req, res) {
+ /* res.send({user1:"sad_man", comment: "he went with another man"}); */
+  res.render('about', {title: 'About you'});
 });
 
 module.exports = router;
